1274223368dd591963bab16691e51581e9da985f
Julianne Kim <juliannne@gmail.com>
2017-11-03 18:05:40 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,11 @@ INSERT INTO series (id, title, authod_id, subgenre_id) VALUES
 INSERT INTO books (id, title, year, series_id) VALUES
 ()
 
-INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES 
+INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES
+()
+
+INSERT INTO subgenres (id, name) VALUES 
+()
+
+INSERT INTO authors (id, name) VALUES 
 ()
