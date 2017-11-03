ec61d4b0ffe808ced90c8e016a168c06c5f817b5
Julianne Kim <juliannne@gmail.com>
2017-11-03 18:15:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,10 @@ INSERT INTO series (id, title, authod_id, subgenre_id) VALUES
 (1, "A Song of Ice and Fire", 1, 1), (2, "Harry Potter Series", 2, 2);
 
 INSERT INTO books (id, title, year, series_id) VALUES
-()
+(1, "A Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), 
+(4, "Harry Potter and the Sorcerer's Stone", 1997, 2), (5, "Harry Potter and the Chamber of Secrets", 1998, 2),
+(6, "Harry Potter and the Prisoner of Azkaban", 1999, 2);
+
 
 INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES
 ()
