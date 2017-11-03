b7cf77ade0c61c5ba56d1674b5331cebd8e6c6d8
Julianne Kim <juliannne@gmail.com>
2017-11-03 18:04:23 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,8 @@
-INSERT INTO series (id, title, authod_id, subgenre_id) VALUES 
+INSERT INTO series (id, title, authod_id, subgenre_id) VALUES
 (1, "Dragon Heart", )
 
-INSERT INTO books (id, title, year, series_id) VALUES 
+INSERT INTO books (id, title, year, series_id) VALUES
+()
+
+INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES 
 ()
