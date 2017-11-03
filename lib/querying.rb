359ecd0b1c9667beb47a5670832b8045a77ae61c
Julianne Kim <juliannne@gmail.com>
2017-11-03 18:58:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,8 @@ def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.id) AS total
   FROM characters
   GROUP BY characters.species
-  HAVING MAX(total);"
+  ORDER BY total DESC 
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
