2a484793fe2b3433109f793160866b705fddb740
Julianne Kim <juliannne@gmail.com>
2017-11-03 19:01:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors 
+  JOIN characters ON authors.id = characters.author_id
+  JOIN series ON characters.series_id = series.id
+  JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
