96407ed685ee1ed0100a0e44d71a9358f4ef1c53
Julianne Kim <juliannne@gmail.com>
2017-11-03 19:10:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,6 +42,5 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.title)
   FROM characters
-  JOIN books ON characters.series_id = books.series_id
-  GROUP BY books.series_id;"
+  JOIN books ON characters.series_id = books.series_id;"
 end
