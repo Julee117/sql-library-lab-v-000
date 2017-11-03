46d73d9226678671b3f76200a46957815e8a460c
Julianne Kim <juliannne@gmail.com>
2017-11-03 18:10:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO series (id, title, authod_id, subgenre_id) VALUES
-(1, "Dragon Heart", )
+(1, "A Song of Ice and Fire", 1, 1),
 
 INSERT INTO books (id, title, year, series_id) VALUES
 ()
@@ -7,8 +7,8 @@ INSERT INTO books (id, title, year, series_id) VALUES
 INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES
 ()
 
-INSERT INTO subgenres (id, name) VALUES 
-()
+INSERT INTO subgenres (id, name) VALUES
+(1, "fantasy"), (2, "fantasy");
 
-INSERT INTO authors (id, name) VALUES 
-()
+INSERT INTO authors (id, name) VALUES
+(1, "George R. R. Martin"), (2, "J.K. Rowling")
